af9f44b8c28ae09942360d7e877f73e5cbccb3c9
shenshuwen <1044042600@qq.com>
2016-03-29 00:28:50 +0800

    add maven project

---
+++ src/main/java/com/kellonge/demo/snippets/java/App.java
@@ -0,0 +1,13 @@
+package com.kellonge.demo.snippets.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
