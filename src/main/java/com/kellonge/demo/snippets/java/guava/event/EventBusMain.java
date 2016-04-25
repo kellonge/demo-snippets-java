73ef380df42d2442f9220f5ae686c527d7d06428
shenshuwen <1044042600@qq.com>
2016-04-25 10:19:09 +0800

    add tread log

--- src/main/java/com/kellonge/demo/snippets/java/guava/event/EventBusMain.java
+++ src/main/java/com/kellonge/demo/snippets/java/guava/event/EventBusMain.java
@@ -1,11 +1,11 @@
 package com.kellonge.demo.snippets.java.guava.event;
 
 public class EventBusMain {
-	public static void main(String[] args) {
-		for (int i = 0; i < 6; i++) {
-			EventBus.post("scs");
-			EventBus.post(1.01D);
-		}
-		System.out.println(11);
-	}
+    public static void main(String[] args) {
+        for (int i = 0; i < 6; i++) {
+            EventBus.post("scs");
+            EventBus.post(1.01D);
+        }
+        System.out.println("thread[" + Thread.currentThread().getName() + "] post finish");
+    }
 }
--- src/main/java/com/kellonge/demo/snippets/java/guava/event/EventBusSubscriber.java
+++ src/main/java/com/kellonge/demo/snippets/java/guava/event/EventBusSubscriber.java
@@ -12,21 +12,24 @@ import com.google.common.eventbus.Subscribe;
  */
 public class EventBusSubscriber {
 
-	@Subscribe
-	public void handleStringEvent(String str) throws Exception {
-		Thread.sleep(100L);
-		System.out.println(new Date() + "@handleStringEvent:" + str);
-	}
+    @Subscribe
+    public void handleStringEvent(String str) throws Exception {
+        Thread.sleep(100L);
+        System.out
+            .println("thread[" + Thread.currentThread().getName() + "] " + new Date() + "@handleStringEvent:" + str);
+    }
 
-	@Subscribe
-	public void handleDoubleEvent(Double d) throws Exception{
-		Thread.sleep(100L);
-		System.out.println(new Date() + "@handleDoubleEvent:" + d);
-	}
-	
-	@Subscribe
-	public void handleDoubleEvent1(Double d) throws Exception{
-		Thread.sleep(100L);
-		System.out.println(new Date() + "@handleDoubleEvent1:" + d);
-	}
+    @Subscribe
+    public void handleDoubleEvent(Double d) throws Exception {
+        Thread.sleep(100L);
+        System.out
+            .println("thread[" + Thread.currentThread().getName() + "] " + new Date() + "@handleDoubleEvent:" + d);
+    }
+
+    @Subscribe
+    public void handleDoubleEvent1(Double d) throws Exception {
+        Thread.sleep(100L);
+        System.out
+            .println("thread[" + Thread.currentThread().getName() + "] " + new Date() + "@handleDoubleEvent1:" + d);
+    }
 }
