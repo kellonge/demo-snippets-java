a13174a4b797300909dcfa46f29a964b618e5ae5
shenshuwen <1044042600@qq.com>
2016-04-28 14:31:44 +0800

    增加org.apache.commons.codec base64示例

--- src/main/java/com/kellonge/demo/snippets/java/base/Base64Main.java
+++ src/main/java/com/kellonge/demo/snippets/java/base/Base64Main.java
@@ -5,15 +5,30 @@ import java.util.Base64;
 /**
  * base64 编码、反编码
  * 使用 java.util
+ * 使用 org.apache.commons.codec
  * 
  * @author kellonge
  * @version $Id: Base64Main.java, v 0.1 2016年4月25日 下午9:35:46 kellonge Exp $
  */
 public class Base64Main {
     public static void main(String[] args) {
-        String raw = "384";
-        byte[] encode = Base64.getEncoder().encode(raw.getBytes());
-        System.out.println(new String(encode));
-        System.out.println(new String(Base64.getDecoder().decode(encode)));
+        javaBase64();
+        apacheBase64();
+    }
+
+    private static void javaBase64() {
+        String raw = "326,327,328,329:1";
+        String encode = new String(Base64.getEncoder().encode(raw.getBytes()));
+        System.out.println("javaBase64 raw:" + raw + " encode:" + encode);
+        String decode = new String(Base64.getDecoder().decode(encode));
+        System.out.println("javaBase64 encode:" + encode + " decode:" + decode);
+    }
+
+    private static void apacheBase64() {
+        String raw = "326,327,328,329:1";
+        String encode = org.apache.commons.codec.binary.Base64.encodeBase64String(raw.getBytes());
+        System.out.println("javaBase64 raw:" + raw + " encode:" + encode);
+        String decode = new String(org.apache.commons.codec.binary.Base64.decodeBase64(encode));
+        System.out.println("javaBase64 encode:" + encode + " decode:" + decode);
     }
 }
